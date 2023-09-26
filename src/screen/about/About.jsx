83f9eb42f93fcebbb623e85cd564a8e174f4f675
jamesmiayo <35914986+jamesmiayo@users.noreply.github.com>
2023-09-26 16:13:22 +0800

    2nd commit

---
+++ src/screen/about/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './about.css';
+
+function About() {
+  return (
+    <div className="container about__container">
+      <div className="title">
+        <h2>THEY ARE READY TO GO! </h2>
+        <h1>Kevin & Nikkis'</h1>
+        <h1> Despedida Party</h1>
+        <h3>October 14, 2023 | 4:00 PM</h3>
+        <h3>Monalisa Kitchen</h3>
+        <h4>(98 E Dela Paz St., San Roque, Marikina City)</h4>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/screen/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__copyright">
+          <small>&copy; SeeKeR | Narmie. All rights reserved</small>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
