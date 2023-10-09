67bb2f5bf316d904943a96352cb278cf12727758
jamesmiayo <35914986+jamesmiayo@users.noreply.github.com>
2023-10-09 21:05:11 +0800

    v3

--- src/screen/navigation/Nav.jsx
+++ src/screen/navigation/Nav.jsx
@@ -121,6 +121,7 @@ const Nav = () => {
         </div>
         <div className={toggleState === 2 ? 'active-content' : 'content'}>
           <div className="map">
+            <h2>Map to MONALISA KITCHEN</h2>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d482.5653744636661!2d121.0978207903299!3d14.626227869289101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b8373c9418d1%3A0x6287306b864307eb!2s98%20E%20dela%20Paz%20St%2C%20Marikina%2C%201800%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1695806142550!5m2!1sen!2sph"
               width="100%"
@@ -135,6 +136,7 @@ const Nav = () => {
           </div>
 
           <div className="map2">
+            <h2>Map to PARKING</h2>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d341.2253862183919!2d121.09779210213009!3d14.626139354716202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b9c1fd1b39c5%3A0xba0fea3f975bc2b!2sMonalisa%20Kitchen%20Parking!5e0!3m2!1sen!2sph!4v1696856120011!5m2!1sen!2sph"
               width="100%"
